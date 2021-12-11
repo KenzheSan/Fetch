ba93f08e19874e86fcc1e4cab64d1551fa0aa229
KenzheSan <js3nursultan@gmial.com>
2021-12-11 15:14:27 +0600

    Hard-work

--- src/App.js
+++ src/App.js
@@ -1,45 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import useHttp from './hooks/use-https'
-import Tasks from './components/Tasks/Tasks'
-import NewTask from './components/NewTask/NewTask'
-
 function App() {
-	const [tasks, setTasks] = useState([])
-
-	const { error, isLoading, sendRequest: fetchTasks } = useHttp()
-
-	useEffect(() => {
-		const transformTasks = (taskObj) => {
-			const loadedTasks = []
-
-			for (const taksKey in taskObj) {
-				loadedTasks.push({ id: taksKey, text: taskObj[taksKey].text })
-			}
-			setTasks(loadedTasks)
-		}
-		fetchTasks(
-			{
-				url: 'https://react-http-test-a5917-default-rtdb.firebaseio.com/tasks.json',
-			},
-			transformTasks,
-		)
-	}, [fetchTasks])
-
-	const taskAddHandler = (task) => {
-		setTasks((prevTasks) => prevTasks.concat(task))
-	}
-
-	return (
-		<React.Fragment>
-			<NewTask onAddTask={taskAddHandler} />
-			<Tasks
-				items={tasks}
-				loading={isLoading}
-				error={error}
-				onFetch={fetchTasks}
-			/>
-		</React.Fragment>
-	)
+	return <div></div>
 }
 
 export default App
